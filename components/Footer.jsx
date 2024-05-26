1cb0a8fbb12a64433120d6f9e303444c1913eb74
simplyComplex <91500514+Simplycomplex64@users.noreply.github.com>
2024-05-25 22:32:03 -0400

    second commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Footer(){
     return(
         <div className="footer-section-container">
-            <img src="../images/TwitterIcon.png"/>
-            <img src="../images/FacebookIcon.png"/>
-            <img src="../images/InstagramIcon.png"/>
-            <img src="../images/GitHubIcon.png"/>
+            <img src="../images/TwitterIcon.png" alt="twitter logo/icon"/>
+            <img src="../images/FacebookIcon.png" alt="facebook icon"/>
+            <img src="../images/InstagramIcon.png" alt="instagram  icon"/>
+            <img src="../images/GitHubIcon.png" alt="git hub icon"/>
         </div>
     )
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -3,21 +3,25 @@ import React from "react";
 export default function Header(){
     return(
         <div className="info-container">
-            <img className="my-image" src="./src/assets/business-card-picture2.png" />
+            <img className="my-image" src="./src/assets/business-card-picture2.png" alt="picture of Joachim James smiling at the camera."/>
                 <div className="my-infos-container">
                          <h2>Joachim James</h2>
                          <p>Frontend Developer</p>
                          <p id="website-label">simplycomplex.website</p>
                 </div>
                     <div className="my-buttons-container">
-                         <button>
-                            <img className="email-icon" src="./images/emailIcon.png"/>
+                        <a href="mailto:simplycomplex64@gmail.com">
+                        <button>
+                            <img className="email-icon" src="./images/emailIcon.png" alt="contact via email icon"/>
                             <p>E-mail</p>
                             </button>
-                         <button className="linkedIn-btn">
-                         <img className="linkedIn-icon" src="./images/linkedIn.png"/>
+                        </a>
+                        <a href="https://linkedin.com/in/james-joachim-565890289">
+                        <button className="linkedIn-btn">
+                         <img className="linkedIn-icon" src="./images/linkedIn.png" alt="link to Joachim James linked in account."/>
                             LinkedIn
                             </button>
+                        </a>
                 </div>
         </div>
     )
