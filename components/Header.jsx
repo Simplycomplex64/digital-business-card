0f3e4084aa2ae5f89e1c3ca8918f0bfdb4bb5bf1
simplyComplex <91500514+Simplycomplex64@users.noreply.github.com>
2024-05-25 22:56:53 -0400

    3rd commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header(){
     return(
         <div className="info-container">
-            <img className="my-image" src="./src/assets/business-card-picture2.png" alt="picture of Joachim James smiling at the camera."/>
+            <img className="my-image" src="./public/business-card-picture2.png" alt="picture of Joachim James smiling at the camera."/>
                 <div className="my-infos-container">
                          <h2>Joachim James</h2>
                          <p>Frontend Developer</p>
@@ -12,13 +12,13 @@ export default function Header(){
                     <div className="my-buttons-container">
                         <a href="mailto:simplycomplex64@gmail.com">
                         <button>
-                            <img className="email-icon" src="./images/emailIcon.png" alt="contact via email icon"/>
+                            <img className="email-icon" src="./public/emailIcon.png" alt="contact via email icon"/>
                             <p>E-mail</p>
                             </button>
                         </a>
                         <a href="https://linkedin.com/in/james-joachim-565890289">
                         <button className="linkedIn-btn">
-                         <img className="linkedIn-icon" src="./images/linkedIn.png" alt="link to Joachim James linked in account."/>
+                         <img className="linkedIn-icon" src="./public/linkedIn.png" alt="link to Joachim James linked in account."/>
                             LinkedIn
                             </button>
                         </a>
